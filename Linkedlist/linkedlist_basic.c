58f771c67293c5d5090b7194b156b2bf1937b6c2
Woohyuk Lee <woohlee@ttu.edu>
2025-10-17 10:37:45 -0500

    Fix: remove submodule; add Linkedlist as regular folder

---
+++ Linkedlist/linkedlist_basic.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+}Node;
+
+Node *head = NULL;
+
+void InsertAtHead(int data) {
+    Node *temp = malloc(sizeof(Node));
+    temp->data = data;
+    temp->next = NULL;
+    if(head == NULL) {
+        head = temp;
+        return;
+    }
+    temp->next = head;
+    head = temp;
+}
+
+void InsertAtPosition(int data, int n) {
+    Node *temp = malloc(sizeof(Node));
+    temp->data = data;
+    temp->next = NULL;
+    if (head == NULL || n == 1) {
+        temp->next = head;
+        head = temp;
+        return;
+    }
+    Node *cur = head;
+    for(int i = 0; i < n-2; i++) {
+        if(cur->next == NULL) break;
+        cur = cur->next;
+    }
+    temp->next = cur->next;
+    cur->next = temp;
+    return;
+}
+
+void InsertAtTail(int data) {
+    Node *temp = malloc(sizeof(Node));
+    temp->data = data;
+    temp->next = NULL;
+    if(head == NULL) {
+        head = temp;
+        return;
+    }
+    Node *cur = head;
+    while(cur->next != NULL) {
+        cur = cur->next;
+    }
+    cur->next = temp;
+    return;
+}
+
+
+void DeleteAtPosition(int n) {
+    if(head == NULL || n <= 0) return;
+
+    if(n == 1) {
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        return;
+    }
+        Node *temp;
+        Node *cur = head;
+        for(int i = 0; i < n-2; i++) {
+            if(cur->next == NULL) return;
+            cur = cur->next;
+        }
+        temp = cur->next;
+        if(temp == NULL) return;
+        cur->next = temp->next;
+        free(temp);
+}
+
+void DeleteByValue(int value) {
+    if(head == NULL) return;
+    if(head->data == value) {
+        Node *tmp = head;
+        head = head->next;
+        free(tmp);
+        return;
+ }    
+    Node *cur = head;
+    while(cur->next != NULL) {
+        if(cur->next->data == value) {
+            Node *tmp = cur->next;
+            cur->next = tmp->next;
+            free(tmp);
+            return;
+        }
+        cur = cur->next;
+    }
+    return;
+}
+
+void Reverse() {
+    if(!head) return;
+    Node *prev = NULL, *cur = head, *next = NULL;
+
+    while(cur != NULL) {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    head = prev;
+}
+void PrintList() {
+    if(head == NULL) {
+        printf("There is no LinkedList\n");
+        return;
+    }
+    Node *cur = head;
+    while(cur != NULL) {
+        printf("%d ", cur->data);
+        cur = cur->next;
+    }
+    printf("\n");
+}
+
+int Find(int value) {
+    if(head == NULL) return -1;
+    Node *cur = head;
+    int cnt = 1;
+    while(cur) {
+        if(value == cur->data) return cnt;        
+        cnt++;
+        cur = cur->next;
+    }
+
+    return -1;
+}
+
+int Length() {
+    int cnt = 0;
+    Node *cur = head;
+    while(cur) {
+        cur = cur->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+void FreeList() {
+    Node *cur = head;
+    while(cur) {
+        Node *temp = cur;
+        cur = cur->next;
+        free(temp);
+    }
+    head = NULL;
+    return;
+}
+
+int main() {
+    InsertAtHead(2);
+    InsertAtHead(4);
+    InsertAtHead(6);
+    InsertAtPosition(5, 2);
+    InsertAtPosition(7, 1);
+
+    DeleteAtPosition(3);
+
+    PrintList();
+    
+    printf("%d\n", Length());
+    printf("%d\n", Find(4));
+    FreeList();
+
+    return 0;
+}
